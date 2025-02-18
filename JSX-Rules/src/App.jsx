fed0ac043d860ea9c8d843b9ca90a2cf0acd4854
Tanmai7 <tanmaisodisetti@gmail.com>
2025-02-18 20:35:59 +0530

    commit

---
+++ JSX-Rules/src/App.jsx
@@ -0,0 +1,11 @@
+import JSXRules from "./JSXRules";
+
+function App() {
+  return (
+    <div>
+      <JSXRules />
+    </div>
+  );
+}
+
+export default App;
---
+++ JSX-Rules/src/JSXRules.jsx
@@ -0,0 +1,16 @@
+function JSXRules() {
+    return (
+      <div>
+        <h1>JSX Rules</h1>
+        <p>Here are some important RULES of JSX:</p>
+        <ul>
+          <li>JSX must return a <strong>single parent element</strong>.</li>
+          <li>JSX elements must be <strong>properly closed</strong>.</li>
+          <li>JSX attributes are written using <strong>camelCase</strong> (e.g., <code>className</code> instead of <code>class</code>).</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default JSXRules;
+  
